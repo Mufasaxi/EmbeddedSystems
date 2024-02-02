4d5723285596fa474a983a91a96b8bfb3355234d
Mustafa Nazim Salih Abdalla <ma28@tu-clausthal.de>
2024-02-02 16:53:36 +0100

    Add modified solution to 3.3.2

--- src/main.c
+++ src/main.c
@@ -2,11 +2,6 @@
 #include "stm32f4xx_hal_i2c.h"
 #include "stdio.h"
 
-#define LED_PIN_1 GPIO_PIN_5
-#define LED_PIN_2 GPIO_PIN_6
-#define LED_PIN_3 GPIO_PIN_7
-#define LED_PIN_4 GPIO_PIN_6
-
 void LED_Init();
 void Sen_Init();
 
@@ -15,68 +10,39 @@ void SysTick_Handler(void)
     HAL_IncTick();
 }
 
-const uint8_t BH1750_POWER_ON = 0x01;
-const uint8_t BH1750_RESET = 0x07;
-const uint8_t BH1750_CONTINUOUS_HIGH_RES_MODE = 0x10;
-
-void displayLux(float percentage)
+void luxLED(float intensity)
 {
-    if (percentage < 0.2f)
+    if (intensity < 0.2f)
     {
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_1, GPIO_PIN_SET);
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_2, GPIO_PIN_SET);
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_3, GPIO_PIN_SET);
-        // HAL_GPIO_WritePin(GPIOB, LED_PIN_4, GPIO_PIN_SET);
-
         GPIOA->ODR |= (1 << 5);
         GPIOA->ODR |= (1 << 6);
         GPIOA->ODR |= (1 << 7);
         GPIOB->ODR |= (1 << 6);
 
     }
-    else if (percentage >= 0.2f && percentage < 0.4f)
+    else if (intensity >= 0.2f && intensity < 0.4f)
     {
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_1, GPIO_PIN_SET);
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_2, GPIO_PIN_SET);
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_3, GPIO_PIN_SET);
-        // HAL_GPIO_WritePin(GPIOB, LED_PIN_4, GPIO_PIN_RESET);
-
         GPIOA->ODR |= (1 << 5);
         GPIOA->ODR |= (1 << 6);
         GPIOA->ODR |= (1 << 7);
         GPIOB->ODR &= ~(1 << 6);
     }
-    else if (percentage >= 0.4f && percentage < 0.6f)
+    else if (intensity >= 0.4f && intensity < 0.6f)
     {
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_1, GPIO_PIN_SET);
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_2, GPIO_PIN_SET);
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_3, GPIO_PIN_RESET);
-        // HAL_GPIO_WritePin(GPIOB, LED_PIN_4, GPIO_PIN_RESET);
-
         GPIOA->ODR |= (1 << 5);
         GPIOA->ODR |= (1 << 6);
         GPIOA->ODR &= ~(1 << 7);
         GPIOB->ODR &= ~(1 << 6);
     }
-    else if (percentage >= 0.6f && percentage < 0.8f)
+    else if (intensity >= 0.6f && intensity < 0.8f)
     {
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_1, GPIO_PIN_SET);
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_2, GPIO_PIN_RESET);
-        // HAL_GPIO_WritePin(GPIOA, LED_PIN_3, GPIO_PIN_RESET);
-        // HAL_GPIO_WritePin(GPIOB, LED_PIN_4, GPIO_PIN_RESET);
-
         GPIOA->ODR |= (1 << 5);
         GPIOA->ODR &= ~(1 << 6);
         GPIOA->ODR &= ~(1 << 7);
         GPIOB->ODR &= ~(1 << 6);
     }
-    else if (percentage >= 0.8f)
+    else if (intensity >= 0.8f)
     {
-    //     HAL_GPIO_WritePin(GPIOA, LED_PIN_1, GPIO_PIN_RESET);
-    //     HAL_GPIO_WritePin(GPIOA, LED_PIN_2, GPIO_PIN_RESET);
-    //     HAL_GPIO_WritePin(GPIOA, LED_PIN_3, GPIO_PIN_RESET);
-    //     HAL_GPIO_WritePin(GPIOB, LED_PIN_4, GPIO_PIN_RESET);
-
         GPIOA->ODR &= ~(1 << 5);
         GPIOA->ODR &= ~(1 << 6);
         GPIOA->ODR &= ~(1 << 7);
@@ -110,70 +76,28 @@ int main(void)
         return 0;
     }
 
-    uint8_t addr = 0x23<<1;// 0u;
+    // To get a whole Byte
+    uint8_t addr = 0x23<<1;
 
-    uint8_t buff[2];
-
-    // for (uint8_t address = 0x03u; address < 0x78u; address++)
-    // {
-    //     if (HAL_I2C_IsDeviceReady(&hI2C, address << 1, 3, 10) == HAL_OK)
-    //     {
-    //         addr = address;
-    //     }
-    // }
-
-    uint16_t nLUX = 0;
-    uint8_t *pBuffer = (uint8_t *)&nLUX;
+    // Create pointer buff to get bytes of lux
+    uint16_t lux = 0;
+    uint8_t *buff = (uint8_t *)&lux;
 
     // Continously H Res
     buff[0] = 0b00010000;
-    pBuffer[0] = 0b00010000;
 
-    HAL_I2C_Master_Transmit(&hI2C, addr, pBuffer, 1, HAL_MAX_DELAY);
+    HAL_I2C_Master_Transmit(&hI2C, addr, buff, 1, HAL_MAX_DELAY);
     HAL_Delay(150);
 
     while (1) {
-      HAL_I2C_Master_Receive(&hI2C, addr, pBuffer, 2, HAL_MAX_DELAY);
+      HAL_I2C_Master_Receive(&hI2C, addr, buff, 2, HAL_MAX_DELAY);
       
-      float grade = nLUX / 65535.0f;
+      // To normalise
+      float intensity = lux/ 65535.0f;
       HAL_Delay(150);
 
-      displayLux(grade);
+      luxLED(intensity);
     }
-
-    // if (addr != 0)
-    // {
-    //     pBuffer[0] = BH1750_POWER_ON;
-    //     if (HAL_I2C_Master_Transmit(&hI2C, addr << 1, pBuffer, 1, 100) != HAL_OK)
-    //     {
-    //         return 1;
-    //     }
-    //     HAL_Delay(5);
-
-    //     pBuffer[0] = BH1750_RESET;
-    //     if (HAL_I2C_Master_Transmit(&hI2C, addr << 1, pBuffer, 1, 100) != HAL_OK)
-    //     {
-    //         return 1;
-    //     }
-
-    //     pBuffer[0] = BH1750_CONTINUOUS_HIGH_RES_MODE;
-    //     if (HAL_I2C_Master_Transmit(&hI2C, addr << 1, pBuffer, 1, 100) != HAL_OK)
-    //     {
-    //         return 1;
-    //     }
-    //     HAL_Delay(180);
-
-    //     while (1)
-    //     {
-    //         // Read measurement
-    //         if (HAL_I2C_Master_Receive(&hI2C, addr << 1, pBuffer, 2, 100) != HAL_OK)
-    //             continue;
-
-    //         float grade = nLUX / 65535.0f;
-    //         HAL_Delay(120);
-    //         displayLux(grade);
-    //     }
-    // }
 
     while (1)
     {
@@ -192,36 +116,9 @@ void Sen_Init()
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
 
-// void LED_Init_A(int N)
-// {
-//     GPIOA->MODER &= ~(3 << (N * 2)); // mode[1:0] für pin N zurücksetzen
-//     GPIOA->MODER |= (1 << (N * 2));  // mode[1:0] für pin N auf 01 (GP output) setzen
-
-//     GPIOA->OTYPER &= ~(1 << N); // type für pin N zurücksetzen (push-pull)
-
-//     GPIOA->OSPEEDR &= ~(3 << (N * 2)); // speed[1:0] für pin N zurücksetzen (low speed)
-
-//     GPIOA->PUPDR &= ~(3 << (N * 2)); // pupdr[1:0] für pin N zurücksetzen (GP output + push-pull)
-// }
-
-// void LED_Init_B(int N)
-// {
-//     GPIOB->MODER &= ~(3 << (N * 2)); // mode[1:0] für pin N zurücksetzen
-//     GPIOB->MODER |= (1 << (N * 2));  // mode[1:0] für pin N auf 01 (GP output) setzen
-
-//     GPIOB->OTYPER &= ~(1 << N); // type für pin N zurücksetzen (push-pull)
-
-//     GPIOB->OSPEEDR &= ~(3 << (N * 2)); // speed[1:0] für pin N zurücksetzen (low speed)
-
-//     GPIOB->PUPDR &= ~(3 << (N * 2)); // pupdr[1:0] für pin N zurücksetzen (GP output + push-pull)
-// }
 
 void LED_Init()
 {
-    // LED_Init_A(5);
-    // LED_Init_A(6);
-    // LED_Init_A(7);
-    // LED_Init_B(6);
   GPIOA->MODER &= ~(3 << (5 * 2));
   GPIOA->MODER |= (1 << (5 * 2));
   GPIOA->PUPDR &= ~(3 << (5 * 2));
@@ -241,7 +138,7 @@ void LED_Init()
   GPIOB->MODER |= (1 << (6 * 2));
   GPIOB->PUPDR &= ~(3 << (6 * 2));
   GPIOB->OSPEEDR &= ~(3 << (6 * 2));
-  // Turn on all LEDS  (AT THE START THEY'RE ALL ON EXPECT D4 THEN THEY START TURNING OFF SEQUENTIALLY AND IN THE NEXT CYCLE THEY TURN ON AS EXPECTED)
+  // Turn off all LEDS
   GPIOA->ODR &= ~(1 << 5);
   GPIOA->ODR &= ~(1 << 6);
   GPIOA->ODR &= ~(1 << 7);
