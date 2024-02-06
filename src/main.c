e24e9d98f706e969927961c9f47d3c5543943174
Mustafa <mustafalbahar@hotmail.com>
2024-02-06 15:05:46 +0100

    Added final solution to 3.3.2

--- src/main.c
+++ src/main.c
@@ -5,18 +5,6 @@
 void LED_Init();
 void Sen_Init();
 
-void Reg_Init();
-void sendToSeg(uint8_t segNo, uint8_t hexVal);
-void shiftOut(uint8_t bitOrder, uint8_t value);
-void displayHex(uint16_t hexValue);
-
-uint8_t patterns[16] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90, 0x88, 0x83, 0xC6, 0xA1, 0x84, 0x8E};
-
-uint8_t select_seg1 = 0xF1;
-uint8_t select_seg2 = 0xF2;
-uint8_t select_seg3 = 0xF4;
-uint8_t select_seg4 = 0xF8;
-
 void SysTick_Handler(void)
 {
   HAL_IncTick();
@@ -24,71 +12,35 @@ void SysTick_Handler(void)
 
 void luxLED(float intensity)
 {
-  // if (intensity < 0.2f)
-  // {
-  //     GPIOA->ODR |= (1 << 5);
-  //     GPIOA->ODR |= (1 << 6);
-  //     GPIOA->ODR |= (1 << 7);
-  //     GPIOB->ODR |= (1 << 6);
-
-  // }
-  // else if (intensity >= 0.2f && intensity < 0.4f)
-  // {
-  //     GPIOA->ODR |= (1 << 5);
-  //     GPIOA->ODR |= (1 << 6);
-  //     GPIOA->ODR |= (1 << 7);
-  //     GPIOB->ODR &= ~(1 << 6);
-  // }
-  // else if (intensity >= 0.4f && intensity < 0.6f)
-  // {
-  //     GPIOA->ODR |= (1 << 5);
-  //     GPIOA->ODR |= (1 << 6);
-  //     GPIOA->ODR &= ~(1 << 7);
-  //     GPIOB->ODR &= ~(1 << 6);
-  // }
-  // else if (intensity >= 0.6f && intensity < 0.8f)
-  // {
-  //     GPIOA->ODR |= (1 << 5);
-  //     GPIOA->ODR &= ~(1 << 6);
-  //     GPIOA->ODR &= ~(1 << 7);
-  //     GPIOB->ODR &= ~(1 << 6);
-  // }
-  // else if (intensity >= 0.8f)
-  // {
-  //     GPIOA->ODR &= ~(1 << 5);
-  //     GPIOA->ODR &= ~(1 << 6);
-  //     GPIOA->ODR &= ~(1 << 7);
-  //     GPIOB->ODR &= ~(1 << 6);
-  // }
-  if (intensity < 0.2f)
+  if (intensity < 1000.0f)
   {
     GPIOA->ODR |= (1 << 5);
     GPIOA->ODR |= (1 << 6);
     GPIOA->ODR |= (1 << 7);
     GPIOB->ODR |= (1 << 6);
   }
-  else if (intensity >= 1.2f && intensity < 1.4f)
+  else if (intensity >= 1000.0f && intensity < 1200.0f)
   {
     GPIOA->ODR |= (1 << 5);
     GPIOA->ODR |= (1 << 6);
     GPIOA->ODR |= (1 << 7);
     GPIOB->ODR &= ~(1 << 6);
   }
-  else if (intensity >= 1.4f && intensity < 1.6f)
+  else if (intensity >= 1200.0f && intensity < 1400.0f)
   {
     GPIOA->ODR |= (1 << 5);
     GPIOA->ODR |= (1 << 6);
     GPIOA->ODR &= ~(1 << 7);
     GPIOB->ODR &= ~(1 << 6);
   }
-  else if (intensity >= 1.6f && intensity < 1.8f)
+  else if (intensity >= 1400.0f && intensity < 1600.0f)
   {
     GPIOA->ODR |= (1 << 5);
     GPIOA->ODR &= ~(1 << 6);
     GPIOA->ODR &= ~(1 << 7);
     GPIOB->ODR &= ~(1 << 6);
   }
-  else if (intensity >= 1.8f)
+  else if (intensity >= 1600.0f)
   {
     GPIOA->ODR &= ~(1 << 5);
     GPIOA->ODR &= ~(1 << 6);
@@ -107,7 +59,6 @@ int main(void)
 
   LED_Init();
   Sen_Init();
-  Reg_Init();
 
   I2C_HandleTypeDef hI2C;
   hI2C.Instance = I2C1;
@@ -118,6 +69,7 @@ int main(void)
   hI2C.Init.OwnAddress2 = 0;
   hI2C.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
   hI2C.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  hI2C.Init.ClockSpeed = 400000;
 
   if (HAL_I2C_Init(&hI2C) != HAL_OK)
   {
@@ -134,21 +86,36 @@ int main(void)
   // Continously H Res
   buff[0] = 0b00010000;
 
-  HAL_I2C_Master_Transmit(&hI2C, addr, buff, 1, HAL_MAX_DELAY);
+  if (HAL_I2C_Master_Transmit(&hI2C, addr, buff, 1, HAL_MAX_DELAY) != HAL_OK)
+  {
+    while (1)
+    {
+    }
+  }
+  // HAL_I2C_Master_Transmit(&hI2C, addr, buff, 1, HAL_MAX_DELAY);
   HAL_Delay(150);
-  uint16_t intensity2;
 
   while (1)
   {
-    // displayHex(0x145);
     HAL_I2C_Master_Receive(&hI2C, addr, buff, 2, HAL_MAX_DELAY);
 
+    // volatile int result = 0;
+    // for (int i = 0; i < 8; i++)
+    // {
+    //   result = (result << 8) + (lux & 0xFF);
+    //   lux >>= 8;
+    // }
+    uint8_t tmp = buff[0];
+    buff[0] = buff[1];
+    buff[1] = tmp;
+
+
     // To normalise
-    float intensity = /*lux/ 1.2f;*/ lux / pow(2,16);//65535.0f;
-    // intensity /= 100000.0f;
-    displayHex(lux);
+    float intensity = lux / 1.2f;//65535.0f;
+    // float intensity = result / 1.2f;
+    HAL_Delay(150);
+
     luxLED(intensity);
-    // HAL_Delay(150);
   }
 
   while (1)
@@ -193,96 +160,4 @@ void LED_Init()
   GPIOA->ODR &= ~(1 << 6);
   GPIOA->ODR &= ~(1 << 7);
   GPIOB->ODR &= ~(1 << 6);
-}
-
-void Reg_Init()
-{
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  // Chip Select
-  GPIOB->MODER &= ~(3 << (5 * 2));
-  GPIOB->MODER |= (1 << (5 * 2));
-  GPIOB->PUPDR &= ~(3 << (5 * 2));
-  GPIOB->OSPEEDR |= (3 << (5 * 2));
-  // Set to 1 initially
-  GPIOB->ODR |= (1 << (5));
-
-  // Clock
-  GPIOA->MODER &= ~(3 << (8 * 2));
-  GPIOA->MODER |= (1 << (8 * 2));
-  GPIOA->PUPDR &= ~(3 << (8 * 2));
-  GPIOA->OSPEEDR |= (3 << (8 * 2));
-  // Set to 0 initially
-  GPIOA->ODR &= ~(1 << (8));
-
-  // SDI / DO
-  GPIOA->MODER &= ~(3 << (9 * 2));
-  GPIOA->MODER |= (1 << (9 * 2));
-  GPIOA->PUPDR &= ~(3 << (9 * 2));
-  GPIOA->OSPEEDR |= (3 << (9 * 2));
-
-  // Additional ADC GPIO configuration (A0)
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-
-  GPIO_InitStruct.Pin = GPIO_PIN_0;
-  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-}
-
-void shiftOut(uint8_t bitOrder, uint8_t value)
-{
-  for (uint8_t i = 0; i < 8; i++)
-  {
-    // MSBFIRST: Send the most significant bit (MSB) first, here as input 1
-    // LSBFIRST: Send the least significant bit (LSB) first, here as input 0
-    uint8_t bit = (bitOrder == 1) ? ((value & 0x80) >> 7) : (value & 0x01);
-
-    // Set data pin
-    if (bit == 1)
-    {
-      GPIOA->ODR |= (1 << 9);
-    }
-    else
-    {
-      GPIOA->ODR &= ~(1 << 9);
-    }
-
-    // Pulse clock (set and reset)
-    GPIOA->ODR |= (1 << 8);
-    GPIOA->ODR &= ~(1 << 8);
-
-    // Shift value to the next bit
-    value = (bitOrder == 1) ? (value << 1) : (value >> 1);
-  }
-}
-
-void sendToSeg(uint8_t segNo, uint8_t hexVal)
-{
-  // Latch down (low)
-  GPIOB->ODR &= ~(1 << 5);
-
-  // Transfer Segmenent data
-  shiftOut(1, hexVal);
-  // Transfer Segmenent data
-  shiftOut(1, segNo);
-
-  // Latch up (high)
-  GPIOB->ODR |= (1 << 5);
-}
-
-void displayHex(uint16_t hexValue)
-{
-  // Extract individual digits
-  uint8_t digit4 = (hexValue >> 12) & 0xF;
-  uint8_t digit3 = (hexValue >> 8) & 0xF;
-  uint8_t digit2 = (hexValue >> 4) & 0xF;
-  uint8_t digit1 = hexValue & 0xF;
-
-  // Display each digit on the 7-segment display
-  sendToSeg(select_seg1, patterns[digit4]);
-  sendToSeg(select_seg2, patterns[digit3]);
-  sendToSeg(select_seg3, patterns[digit2]);
-  sendToSeg(select_seg4, patterns[digit1]);
 }
